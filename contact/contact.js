00cd72791e9843c8c7982dd05410356097ea5471
saro-dev <codersaro@gmail.com>
2024-01-15 20:18:10 +0530

    first commit

--- contact/contact.js
+++ contact/contact.js
@@ -20,3 +20,34 @@ gsap.fromTo('.preloader', {
         });
     }
 });
+document.addEventListener("DOMContentLoaded", function () {
+
+    const sideNav = document.querySelector('.side-nav');
+        const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+    
+        mobileMenuIcon.addEventListener('click', function () {
+            const isOpen = sideNav.style.right === '0px';
+    
+            // Use GSAP for smooth rotation animation
+            gsap.to(mobileMenuIcon, { rotation: isOpen ? 0 : -90, duration: 0.3 });
+    
+            const isMobileOrTablet = window.matchMedia('(max-width: 959px)').matches;
+    
+            if (isMobileOrTablet) {
+                // Toggle the side navigation for mobile and big tablets
+                sideNav.style.right = isOpen ? '-100%' : '0px';
+            }
+        });
+    
+        // Close the side navigation if the window is resized beyond the mobile range
+        window.addEventListener('resize', function () {
+            const isMobileOrTablet = window.matchMedia('(max-width: 959px)').matches;
+    
+            if (!isMobileOrTablet) {
+                sideNav.style.right = '-150px';
+            } else {
+                // If the menu icon is visible, set it to -100%
+                sideNav.style.right = mobileMenuIcon.style.display === 'none' ? '-150px' : '-100%';
+            }
+        });
+    })
